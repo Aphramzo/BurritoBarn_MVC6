907a0790f79fed06ad09e76c993e56edb226a01b
aphramzo <im.t.wal@gmail.com>
2015-02-15 08:46:39 -0700

    Grunt in place and handling less and js file minification.

---
+++ src/BurritoBarn/Gruntfile.js
@@ -0,0 +1,40 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.initConfig({
+        uglify: {
+            dev: {
+                files: {
+                    'wwwroot/app.js': ['scripts/app.js', 'scripts/**/*.js']
+                }
+            }
+        },
+
+        less:{
+            dev:{
+                files:{
+                    'css/site.css' : 'less/site.less'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['scripts/**/*.js'],
+                tasks: ['uglify']
+            },
+            styles: {
+                files: ['less/**/*.less'],
+                tasks: ['less']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['uglify', 'less', 'watch']);
+};
---
+++ src/BurritoBarn/scripts/application.js
@@ -0,0 +1,7 @@
+﻿//This is just a test to see if the grunt tasks are working OK
+
+function Application(name) {
+    this.name = name;
+}
+
+var newApplication = new Application('Burrito Barn!');
